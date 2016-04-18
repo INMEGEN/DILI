09c8bfa9a5ffa2108256122f7bf95470629d36dc
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-04-18 16:19:15 -0500

    patch path to submodule

--- PubMed_mining/nw.sh
+++ PubMed_mining/nw.sh
@@ -5,7 +5,7 @@ set -x
 while read FILE;
 do
     xzcat "$FILE" | \
-        python ~/bibliometrics/mesh_network_from_medline.py - \
+        python ../bibliometrics/mesh_network_from_medline.py - \
         | awk -F "\t" '{if ($3>1) print $0}' | grep -v Animals \
         | grep -v Rats | grep -v Human | xz --stdout - \
                                             > ${FILE%medline.xz}_edgelist.tsv.xz
