fa81bd16e66197f3348eb4e8113294669924830a
Joshua Haase <hahj87@gmail.com>
2016-05-16 17:23:28 -0500

    Generate json from edgelist.

---
+++ PubMed_mining/edgelist2json.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script takes from stdin a tsv containing an edgelist in the form:
+
+node1	node2	weight
+
+And outputs a json containing an equivalent network.
+
+Quits on invalid input.
+"""
+
+from pprint import pprint
+import json
+import sys
+import networkx as nx
+from networkx.readwrite import json_graph
+
+def die():
+	exit(1)
+
+graph = nx.Graph()
+
+for line in sys.stdin:
+	try:
+		node1, node2, weight = line.rsplit("\t")
+		graph.add_edge(node1, node2, w=int(weight))
+	except:
+		die()
+
+data = json_graph.node_link_data(graph)
+pprint(data)
